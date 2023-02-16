2e489456ccd6c06e9a813b658b5beb5b70dd7b1c
Jonathan Delgado <113931071+jrdelgado3@users.noreply.github.com>
2023-02-16 18:40:22 +0000

    rr

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 # remember to fork this and push to github
 
+# f = open('employees.txt', 'r')
+# print(f.read())
+# #read gives us everything 
+# #read line gives us first line
+# # # print(f.readline())
+# # f.close()
+# with open("test.txt", "r") as f:
+#   for line in f:
+#     print(line, end='')
+with open('period7.txt', "w") as f:
+  f.write("this mess is over")
+  
